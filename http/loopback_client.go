6c91829f84a57c6ad4ba9edf0ab76f453a50889a
James Chao <james_chao@cable.comcast.com>
2025-02-11 00:40:32 -0800

    a

--- http/loopback_client.go
+++ http/loopback_client.go
@@ -57,35 +57,25 @@ func NewLoopbackClient(router *mux.Router) *LoopbackClient {
 }
 
 func (c *LoopbackClient) Do(req *http.Request) (*http.Response, error) {
-	match := new(mux.RouteMatch)
-	ok := c.router.Match(req, match)
-	if ok {
-		route := match.Route
-		rw := NewLoopbackResponseWriter()
-		hdl := route.GetHandler()
-		hdl.ServeHTTP(rw, req)
-		// fmt.Printf("req = %v\n", req)
-		// fmt.Printf("status = %v\n", rw.Status())
-		// fmt.Printf("body = %s\n", rw.Body())
+	rw := NewLoopbackResponseWriter()
+	c.router.ServeHTTP(rw, req)
+	// fmt.Printf("req = %v\n", req)
+	// fmt.Printf("status = %v\n", rw.Status())
+	// fmt.Printf("body = %s\n", rw.Body())
 
-		body := rw.Body()
-		// reader := bytes.NewReader(body)
-		// Use io.NopCloser to wrap the reader
-		reader := io.NopCloser(bytes.NewReader(body))
+	body := rw.Body()
+	// reader := bytes.NewReader(body)
+	// Use io.NopCloser to wrap the reader
+	reader := io.NopCloser(bytes.NewReader(body))
 
-		// Create a new http.Response
-		resp := &http.Response{
-			StatusCode: http.StatusOK,
-			Header: http.Header{
-				"Content-Type": []string{"application/json"},
-			},
-			Body:          reader,           // Set the response body
-			ContentLength: int64(len(body)), // Set the content length
-		}
-		return resp, nil
-	}
+	// Create a new http.Response
 	resp := &http.Response{
-		StatusCode: http.StatusNotFound,
+		StatusCode: http.StatusOK,
+		Header: http.Header{
+			"Content-Type": []string{"application/json"},
+		},
+		Body:          reader,           // Set the response body
+		ContentLength: int64(len(body)), // Set the content length
 	}
 	return resp, nil
 }
